186e9366cf3addc5f6296c9d1722a089c2d253ab
Old Li <lish@live.cn>
2023-04-07 16:00:45 +0800

    add BarChart

--- BlazorChartJS/Pages/Bar.razor.js
+++ BlazorChartJS/Pages/Bar.razor.js
@@ -1,17 +1,31 @@
-export function barChart() {
-    var charts = $("div.chart");
-    $(charts[0]).children().remove();
-    $(charts[0]).append($("<canvas />", { id: "myChart" }));
-
-    const ctx = document.getElementById('myChart');
+export function barChart(data) {
+    const ctx = document.getElementById('BarChart');
 
     new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                label: 'My First Dataset',
+                data: data,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
+                ],
                 borderWidth: 1
             }]
         },
