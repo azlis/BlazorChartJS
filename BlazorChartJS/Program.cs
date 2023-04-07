f92f2884cd17988dea08129c0e19650019c8e825
Old Li <lish@live.cn>
2023-04-07 11:23:52 +0800

    添加bootstrapblazor组件和bootstrapblazor.chart图表组件

--- BlazorChartJS/Program.cs
+++ BlazorChartJS/Program.cs
@@ -15,7 +15,7 @@ namespace BlazorChartJS
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
-
+            builder.Services.AddBootstrapBlazor();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
